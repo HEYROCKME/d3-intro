6e451a8cab76290181b7bffb6e7e5dd618e86927
Alwin Nedrum <nedrum@gmail.com>
2022-01-12 14:50:56 +0100

    Lesson 6 added

--- script.js
+++ script.js
@@ -109,30 +109,66 @@
 
 // // lesson 5 Making acxess
 
-let data = [80, 100, 56, 120, 180, 30, 40, 0, 150];
-let svgWidth = 600;
-let svgHeight = 400;
+// let data = [80, 100, 56, 120, 180, 30, 40, 0, 150];
+// let svgWidth = 600;
+// let svgHeight = 400;
 
-let svg = d3.select("svg").attr("width", svgWidth).attr("height", svgHeight);
+// let svg = d3.select("svg").attr("width", svgWidth).attr("height", svgHeight);
 
-let xScale = d3
-  .scaleLinear()
-  .domain([0, d3.max(data)])
-  .range([0, svgWidth]);
+// let xScale = d3
+//   .scaleLinear()
+//   .domain([0, d3.max(data)])
+//   .range([0, svgWidth]);
 
-let yScale = d3
-  .scaleLinear()
-  .domain([0, d3.max(data)])
-  .range([svgHeight, 0]);
+// let yScale = d3
+//   .scaleLinear()
+//   .domain([0, d3.max(data)])
+//   .range([svgHeight, 0]);
 
-let x_axis = d3.axisBottom().scale(xScale);
-let y_axis = d3.axisLeft().scale(yScale);
+// let x_axis = d3.axisBottom().scale(xScale);
+// let y_axis = d3.axisLeft().scale(yScale);
 
-svg.append("g").attr("transform", "translate(50, -20)").call(y_axis);
+// svg.append("g").attr("transform", "translate(50, -20)").call(y_axis);
 
-let x_axisTranslate = svgHeight - 20;
+// let x_axisTranslate = svgHeight - 20;
 
-svg
-  .append("g")
-  .attr("transform", `translate(50, ${x_axisTranslate})`)
-  .call(x_axis);
+// svg
+//   .append("g")
+//   .attr("transform", `translate(50, ${x_axisTranslate})`)
+//   .call(x_axis);
+
+// // Lesson 6 creating SVGs
+
+// let svgWidth = 600,
+//   svgHeight = 500;
+
+// let svg = d3
+//   .select("svg")
+//   .attr("width", svgWidth)
+//   .attr("height", svgHeight)
+//   .attr("class", "svg-container")
+//   .style("background", "salmon");
+
+// let line = svg
+//   .append("line")
+//   .attr("x1", 100)
+//   .attr("x2", 500)
+//   .attr("y1", 50)
+//   .attr("y2", 200)
+//   .attr("stroke", "skyblue")
+//   .attr("stroke-width", 10);
+
+// let rectangle = svg
+//   .append("rect")
+//   .attr("x", 100)
+//   .attr("y", 100)
+//   .attr("width", 200)
+//   .attr("height", 100)
+//   .attr("fill", "pink");
+
+// let circle = svg
+//   .append("circle")
+//   .attr("cx", 200)
+//   .attr("cy", 300)
+//   .attr("r", 80)
+//   .attr("fill", "palegreen");
