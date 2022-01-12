fe475761f32527010c191cc31b07aa11e9c171a6
Alwin Nedrum <nedrum@gmail.com>
2022-01-12 13:54:22 +0100

    Lesson 4

--- script.js
+++ script.js
@@ -26,24 +26,85 @@
 
 // console.log(svg);
 
-// // L3
-let dataset = [80, 100, 56, 120, 180, 30, 40, 120, 160];
+// // // L3
+// let dataset = [80, 100, 56, 120, 180, 30, 40, 120, 160];
 
-let svgWidth = 500,
-  svgHeight = 300,
-  barPadding = 5,
-  barWidth = svgWidth / dataset.length;
-svg = d3.select("svg").attr("width", svgWidth).attr("height", svgHeight);
+// let svgWidth = 500,
+//   svgHeight = 300,
+//   barPadding = 5,
+//   barWidth = svgWidth / dataset.length;
+// svg = d3
+//   .select("svg")
+//   .attr("width", svgWidth)
+//   .attr("height", svgHeight)
+//   .style("background", "skyblue");
 
-let barChart = svg
-  .selectAll("rect")
-  .data(dataset)
-  .enter()
-  .append("rect")
-  .attr("y", (d) => svgHeight - d)
-  .attr("height", (d) => d)
-  .attr("width", barWidth - barPadding)
-  .attr("transform", function (d, i) {
-    var translate = [barWidth * i, 0];
-    return "translate(" + translate + ")";
-  });
+// let barChart = svg
+//   .selectAll("rect")
+//   .data(dataset)
+//   .enter()
+//   .append("rect")
+//   .attr("y", (d) => svgHeight - d)
+//   .attr("height", (d) => d)
+//   .attr("width", barWidth - barPadding)
+//   .attr("transform", function (d, i) {
+//     var translate = [barWidth * i, 0];
+//     return "translate(" + translate + ")";
+//   })
+//   .attr("fill", "pink");
+
+// let text = svg
+//   .selectAll("text")
+//   .data(dataset)
+//   .enter()
+//   .append("text")
+//   .text((d) => d)
+//   .attr("y", (d) => svgHeight - d - 2)
+//   .attr("x", (d, i) => barWidth * i)
+//   .attr("fill", "yellow");
+
+// // L4
+
+// let dataset = [80, 100, 56, 120, 180, 30, 40, 120, 160];
+// dataset = [1, 2, 3, 4, 5, 6];
+
+// let svgWidth = 500,
+//   svgHeight = 300,
+//   barPadding = 5,
+//   barWidth = svgWidth / dataset.length;
+// svg = d3
+//   .select("svg")
+//   .attr("width", svgWidth)
+//   .attr("height", svgHeight)
+//   .style("background", "skyblue");
+
+// let yScale = d3
+//   .scaleLinear()
+//   .domain([0, d3.max(dataset)])
+//   .range([0, svgHeight]);
+
+// let barChart = svg
+//   .selectAll("rect")
+//   .data(dataset)
+//   .enter()
+//   .append("rect")
+//   .attr("y", (d) => svgHeight - yScale(d))
+//   .attr("height", (d) => yScale(d))
+//   .attr("width", barWidth - barPadding)
+//   .attr("transform", function (d, i) {
+//     var translate = [barWidth * i, 0];
+//     return "translate(" + translate + ")";
+//   })
+//   .attr("fill", "pink");
+
+// let text = svg
+//   .selectAll("text")
+//   .data(dataset)
+//   .enter()
+//   .append("text")
+//   .text((d) => d)
+//   .attr("y", (d) => svgHeight - yScale(d) * 0.5)
+//   .attr("x", (d, i) => barWidth * i + barWidth * 0.5)
+//   .attr("fill", "yellow");
+
+// // lesson 5
